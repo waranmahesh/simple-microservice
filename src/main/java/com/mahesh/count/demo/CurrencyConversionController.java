6f455c2319379cca4091502390fd2bde12062fa3
Maheshwaran Chinnasamy <maheshwaran.c@tcs.com>
2020-04-08 22:06:20 +0200

    Simple Service

---
+++ src/main/java/com/mahesh/count/demo/CurrencyConversionController.java
@@ -0,0 +1,33 @@
+package com.mahesh.count.demo;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyConversionController {
+
+  private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+  @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+  public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
+      @PathVariable BigDecimal quantity) {
+
+    Map<String, String> uriVariables = new HashMap<>();
+    uriVariables.put("from", from);
+    uriVariables.put("to", to);
+    logger.info("Test");
+
+    return new CurrencyConversionBean(2L, from, to, new BigDecimal(12), quantity,
+        quantity.multiply(new BigDecimal(14)), 34);
+  }
+
+
+}

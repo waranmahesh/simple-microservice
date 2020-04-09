3b737d60a5c330b0e849fa1aa2215599678dabc1
Maheshwaran Chinnasamy <maheshwaran.c@tcs.com>
2020-04-09 15:08:45 +0200

    Swagger Support

--- src/main/java/com/mahesh/count/demo/CurrencyConversionController.java
+++ src/main/java/com/mahesh/count/demo/CurrencyConversionController.java
@@ -10,13 +10,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@Api(value="Currency Coverter", description="Converting the currency")
 public class CurrencyConversionController {
 
   private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 
   @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+  @ApiOperation(value = "Get target currency with quantity", response = CurrencyConversionBean.class)
   public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
       @PathVariable BigDecimal quantity) {
 
